5671c3ef1fc6d2cd9d075ab5810ecff685b3b091
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-05-17 22:16:48 +0530

    Create tkinter_entry_label.py

---
+++ tkinter_entry_label.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+top=Tk()
+top.geometry("400x400")
+
+def rst():
+	user.set("")
+	passw.set("")
+	repassw.set("")
+
+def chk():
+	if passw.get()!=repassw.get():
+		print("Password mismatch !")
+	else:
+		print("Username is :",user.get())
+		print(passw.get())
+
+user=StringVar()
+passw=StringVar()
+repassw=StringVar()
+
+lb1=Label(top,text="Username").grid(row=4)
+t1=Entry(top,textvariable=user).grid(row=5)
+
+lb2=Label(top,text="Password").grid(row=7)
+t2=Entry(top,show="*",exportselection=0,textvariable=passw).grid(row=8)
+
+lb3=Label(top,text="Re-enter Password").grid(row=12)
+t3=Entry(top,show="*",exportselection=0,textvariable=repassw).grid(row=13)
+
+# exportselection - text within an Entry widget, it is exported to the clipboard.To avoid this exportation, use exportselection = 0.
+# show - To make a password entry show = "*".
+# textvariable - to retrieve the current text from your entry widget,set this option to an instance of the StringVar class.
+
+sbt=Button(top,text="SUBMIT",command=chk).grid(row=17)
+qbt=Button(top,text="QUIT",command=top.quit).grid(row=19)
+rbt=Button(top,text="RESET",command=rst).grid(row=23)
+
+top.mainloop()
